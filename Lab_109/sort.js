4eaa51a01b96113fd30045e4041e151ff340fb5e
ethannicholsgithub <34136453+ethannicholsgithub@users.noreply.github.com>
2019-01-10 00:29:24 -0800

    Update sort.js

--- Lab_109/sort.js
+++ Lab_109/sort.js
@@ -1,4 +1,7 @@
 
+var items = [4, 1, 2, 11, 17, 9, 16, 10, 5, 15, 20, 6, 18, 8, 12, 13, 14, 7, 3, 19];
+
+
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
@@ -13,4 +16,38 @@ function setup() {
 //  The draw function is called @ 30 fps
 function draw() {
 
+}
+
+
+selectionSort();
+
+function swap(items, firstValue, secondValue){
+    var temp = items[firstValue];
+    items[firstValue] = items[secondValue];
+    items[secondValue] = temp;
+}
+
+function selectionSort(items){
+
+    var min;
+
+    for (i=0; i < items.length; i++){
+
+        //set minimum to this position
+        min = i;
+
+        //check the rest of the array to see if anything is smaller
+        for (j=i+1; j < items.length; j++){
+            if (items[j] < items[min]){
+                min = j;
+            }
+        }
+
+        //if the minimum isn't in the position, swap it
+        if (i != min){
+            swap(items, i, min);
+        }
+    }
+
+    console.log(items);
 }
