2d7659a78531c3735cc579dd06da03d41f5a8f43
ethannicholsgithub <795318@seq.org>
2019-01-10 08:51:29 -0800

    d

--- Lab_109/sort.js
+++ Lab_109/sort.js
@@ -9,7 +9,8 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-
+  console.log(items);
+  mySort();
 
 }
 
@@ -18,10 +19,10 @@ function draw() {
 
 }
 
-mySort();
 
 
-function mySort(items){
+
+function mySort(){
 
     var length = items.length
     var min;
