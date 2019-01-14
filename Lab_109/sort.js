61d3bf5c1c015f3e310bb59b39b7d2ec90392ab0
ethannicholsgithub <795318@seq.org>
2019-01-14 08:51:13 -0800

    asdf

--- Lab_109/sort.js
+++ Lab_109/sort.js
@@ -1,7 +1,11 @@
 
-var a = [3,1,4,2,5];
-var b = [3,1,4,2,5];
+var a = [];
+var b = [];
 
+var numOfSelecSort = 0;
+var numOfBubbleSort = 0;
+
+var n = 5
 
 //  This is a comment
 //  The setup function function is called once when your program begins
@@ -10,6 +14,7 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  randomArray(n);
   console.log(a);
   mySort();
   console.log(b);
@@ -22,6 +27,13 @@ function draw() {
 
 }
 
+function randomArray(length){
+  for(var i = 0, i < length; i++){
+    randomNumber = Math.floor(random(1, n))
+    a.push(randomNumber);
+  }
+}
+
 
 
 
@@ -31,23 +43,20 @@ function mySort(){
     var min;
 
     for (i = 0; i < length; i++){
-
         //set minimum to this position
         min = i;
-
         //check the rest of the array to see if anything is smaller
         for (j = (i + 1); j < length; j++){
             if (a[j] < a[min]){
                 min = j;
             }
         }
-
         //if the minimum isn't in the position, swap it
         if (i != min){
             swap(a, i, min);
+            numOfSelecSort = numOfSelecSort + 1;
         }
     }
-
     console.log(a);
 }
 
@@ -58,12 +67,15 @@ function swap(a, firstValue, secondValue){
 }
 
 function bubbleSort(){
-  for(i = 0; i < b.length; i++){
-      if (b[j] > b[j+1]) {
-        var temp = b[j];
-        b[j] = b[j+1];
-        b[j+1] = temp;
+  for(i = b.length - 1; i > 0; i--){
+    for(j = 1; j <= i; j++){
+      if (b[j] < b[j-1]) {
+        var lemp = b[j];
+        b[j] = b[j-1];
+        b[j-1] = lemp;
+        numOfBubbleSort = numOfBubbleSort + 1;
       }
     }
-    console.log(b);
   }
+  console.log(b);
+}
