294b359130901f6ce33b2b169c61f94770e00521
ethannicholsgithub <795318@seq.org>
2019-01-15 08:43:34 -0800

    added counter for number of swaps

--- Lab_109/sort.js
+++ Lab_109/sort.js
@@ -1,11 +1,11 @@
 
-var a = [];
-var b = [];
+var array = [];
+var array2 = [];
 
 var numOfSelecSort = 0;
 var numOfBubbleSort = 0;
 
-var n = 5
+var n = 10
 
 //  This is a comment
 //  The setup function function is called once when your program begins
@@ -15,10 +15,10 @@ function setup() {
   background(5, 5, 5);
   fill(200, 30, 150);
   randomArray(n);
-  console.log(a);
+  console.log(array);
   mySort();
-  console.log(b);
-  bubbleSort();
+  console.log(array2);
+  bubbleSort(n);
 
 }
 
@@ -28,9 +28,11 @@ function draw() {
 }
 
 function randomArray(length){
-  for(var i = 0, i < length; i++){
+  for(var i = 0; i < length; i++){
     randomNumber = Math.floor(random(1, n))
-    a.push(randomNumber);
+    array.push(randomNumber);
+    randomNumber2 = Math.floor(random(1, n))
+    array2.push(randomNumber2);
   }
 }
 
@@ -39,7 +41,7 @@ function randomArray(length){
 
 function mySort(){
 
-    var length = a.length
+    var length = array.length
     var min;
 
     for (i = 0; i < length; i++){
@@ -47,17 +49,17 @@ function mySort(){
         min = i;
         //check the rest of the array to see if anything is smaller
         for (j = (i + 1); j < length; j++){
-            if (a[j] < a[min]){
+            if (array[j] < array[min]){
                 min = j;
             }
         }
         //if the minimum isn't in the position, swap it
         if (i != min){
-            swap(a, i, min);
+            swap(array, i, min);
             numOfSelecSort = numOfSelecSort + 1;
         }
     }
-    console.log(a);
+    console.log(array);
 }
 
 function swap(a, firstValue, secondValue){
@@ -67,15 +69,15 @@ function swap(a, firstValue, secondValue){
 }
 
 function bubbleSort(){
-  for(i = b.length - 1; i > 0; i--){
+  for(i = array2.length - 1; i > 0; i--){
     for(j = 1; j <= i; j++){
-      if (b[j] < b[j-1]) {
-        var lemp = b[j];
-        b[j] = b[j-1];
-        b[j-1] = lemp;
+      if (array2[j] < array2[j-1]) {
+        var lemp = array2[j];
+        array2[j] = array2[j-1];
+        array2[j-1] = lemp;
         numOfBubbleSort = numOfBubbleSort + 1;
       }
     }
   }
-  console.log(b);
+  console.log(array2);
 }
