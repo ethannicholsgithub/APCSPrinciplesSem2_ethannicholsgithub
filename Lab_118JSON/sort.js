ceb78e1514637e11001a688ba6358ceb62598c91
ethannicholsgithub <795318@seq.org>
2019-01-24 08:49:55 -0800

    asdf

--- Lab_118JSON/sort.js
+++ Lab_118JSON/sort.js
@@ -1,6 +1,7 @@
-var data = [];
-function preload(){
-  data = loadJSON("data.json");
+var data;
+
+function loadingData(incomingData) {
+  data = incomingData;
 }
 
 //  This is a comment
@@ -10,6 +11,7 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  loadJSON("data.json", loadingData);
 
 
 }
@@ -21,7 +23,7 @@ function draw() {
 
 function mySort(data){
 
-  var length = data.length
+  var length = data.country.length
   var min;
 
   for (i = 0; i < length; i++){
@@ -29,15 +31,15 @@ function mySort(data){
       min = i;
       //check the rest of the array to see if anything is smaller
       for (j = (i + 1); j < length; j++){
-          if (data.murder[j] < data.murder[min]){
+          if (data.country[j].murder < data.country[min].murder){
               min = j;
           }
       }
       //if the minimum isn't in the position, swap it
       if (i != min){
-          swap(data.murder, i, min);
+          swap(data.country.murder, i, min);
           numOfSelecSort++;
       }
   }
-  console.log(data.murer);
+  console.log(data.country.murder);
 }
