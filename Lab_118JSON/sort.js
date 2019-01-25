62826d389b561a17c57ec75fb4f94bc3734dbc78
ethannicholsgithub <795318@seq.org>
2019-01-25 08:49:28 -0800

    f

--- Lab_118JSON/sort.js
+++ Lab_118JSON/sort.js
@@ -1,9 +1,11 @@
 var data;
+function preload(){
+  loadJSON("country.json", loadingData);
+}
 
 function loadingData(incomingData) {
   data = incomingData;
 }
-
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
@@ -11,10 +13,9 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadJSON("data.json", loadingData);
+  mySort();
+ }
 
-
-}
 
 //  The draw function is called @ 30 fps
 function draw() {
@@ -23,7 +24,10 @@ function draw() {
 
 function mySort(data){
 
-  var length = data.country.length
+  var length;
+  if( data){
+     length = data.crime.length;
+  }
   var min;
 
   for (i = 0; i < length; i++){
@@ -31,15 +35,15 @@ function mySort(data){
       min = i;
       //check the rest of the array to see if anything is smaller
       for (j = (i + 1); j < length; j++){
-          if (data.country[j].murder < data.country[min].murder){
+          if (data.crime[j].Murder < data.crime[min].Murder){
               min = j;
           }
       }
       //if the minimum isn't in the position, swap it
       if (i != min){
-          swap(data.country.murder, i, min);
+          swap(data.crime.Murder, i, min);
           numOfSelecSort++;
       }
   }
-  console.log(data.country.murder);
+ //  console.log(data.crime.Murder);
 }
