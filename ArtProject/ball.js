5679c4c376ccf6499cac94cf4f7e902e9afdaffa
ethannicholsgithub <795318@seq.org>
2019-02-26 08:50:54 -0800

    asdf

--- ArtProject/ball.js
+++ ArtProject/ball.js
@@ -1,34 +1,44 @@
 
 
-  /*
-  **   Ball constructer function
-  **   Ethan Nichols
-  **   Aug 21, 2018
-  */
-  function Ball(locX, locY, rad){
-    // Instance variables
-     this.locX = locX;
-     this.locY = locY;
-     this.speedX = random(-3, 3);
-     this.speedY = random(-3, 3);
-     this.rad = rad;
-    // This function calls other functions
-    this.run = function(){
-      this.checkEdges();
-      this.update();
-    }
-    // This function changes the location of the ball
-    // by adding speed to x and y
-     this.update = function(){
-        this.locX = this.locX + this.speedX;
-        this.locY = this.locY + this.speedY;
-     }
+/*
+**   Ball constructer function
+**   Ethan Nichols
+**   Aug 21, 2018
+*/
+function Ball(location, velocity, radius, col){
 
-     //checkEdges() reverses speed when the ball touches an edge
-     this.checkEdges = function(){
-        if(this.locX < 0) this.speedX = -this.speedX;
-        if(this.locX > width) this.speedX = -this.speedX;
-        if(this.locY < 0) this.speedY = -this.speedY;
-        if(this.locY > height) this.speedY = -this.speedY;
-     }
+  // Instance variables
+  this.loc = location;
+  this.vel = velocity;
+  this.rad = radius;
+  this.col = col;
+  this.acc = createVector(0, .1);
+
+  // This function calls other functions
+  this.run = function(){
+    this.checkEdges();
+    this.update();
+    this.render();
   }
+
+  // This function changes the location of the ball
+  // by adding speed to x and
+  this.update = function(){
+    this.acc.add(this.vel);
+  }
+
+  //checkEdges() reverses speed when the ball touches an edge
+  this.checkEdges = function(){
+    if(this.loc.x < 0) this.vel.x = -this.vel.x;
+    if(this.loc.x > width) this.vel.x = -this.vel.x;
+    if(this.loc.y < 0) this.vel.y = -this.vel.y;
+    if(this.loc.y > height) this.vel.y = -this.vel.y;
+  }
+
+  // render() draws the ball at the new location
+  this.render = function(){
+    fill(this.col);
+    ellipse(this.loc.x, this.loc.y, this.rad, this.rad);
+  }
+
+}
--- ArtProject/sketch.js
+++ ArtProject/sketch.js
@@ -1,27 +1,38 @@
+//  Global variables
+var balls = [];
+var b1;
 
-  //  Global variables
-var circles = [];
-
-var numCircle = 10
-  // setup code
+// put setup code here
 function setup() {
-    var cnv = createCanvas(800, 800);
-    cnv.position((windowWidth-width)/2, 30);
-    background(20,20,20);
-  }
+
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20,20,20);
+  loadBalls(50);
+  b1 = new Ball(createVector(width / 2, height / 2), createVector(0 , 0), 25, color(255, 0, 0));
+
+}
 
 function draw() {
-  for(var i = 0; i < circles.length; i = i + 1){
-      circles[i].run();
-    }
-  }
 
-function loadCircle(numCircle){
-  for(var i = 0; i < numCircle; i++){
-      var loc = createVector(random(width), random(height));
-      var vel = createVector(random(-3,3), random(-3,3));
-      var radius = 20;
-      c1 = new Ball(random(width), random(height), random(15, 35));
-      circles.push(c1);
+  background(20,20,20);
+  b1.run();
+  for(var i = 0; i < balls.length; i = i + 1){
+      balls[i].run();
   }
+}
+
+function loadBalls(numBalls){
+
+    for(var i = 0; i < numBalls; i++){
+
+        var loc = createVector(random(width), random(height));
+        var vel = createVector(random(-3,3), random(-3,3));
+        var radius = random(20,40);
+        var col = color(random(255), random(255), random(255));
+
+        balls.push(new Ball(loc, vel, radius, col));
+
+    }
+
 }
