ab6b996e6a448b8e68aadefd7a76eb54daed69d9
ethannicholsgithub <34136453+ethannicholsgithub@users.noreply.github.com>
2019-02-28 00:01:58 -0800

    added fade and lines

--- ArtProject/sketch.js
+++ ArtProject/sketch.js
@@ -8,14 +8,13 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
-  loadBalls(4);
+  loadBalls(10);
   b1 = new Ball(createVector(width / 2, height / 2), createVector(0 , 0), 25, color(255, 0, 0));
 
 }
 
 function draw() {
-
-  background(20,20,20, 20);
+  background(0,0,0);
   // b1.run();
   for(var i = 0; i < balls.length; i = i + 1){
       balls[i].run();
@@ -28,8 +27,8 @@ function loadBalls(numBalls){
 
         var loc = createVector(random(width), random(height));
         var vel = createVector(random(-3,3), random(-3,3));
-        var radius = random(20, 100);
-        var col = color(random(255), random(255), random(255));
+        var radius = random(50, 150);
+        var col = color(200,75,0);
 
         balls.push(new Ball(loc, vel, radius, col));
 
