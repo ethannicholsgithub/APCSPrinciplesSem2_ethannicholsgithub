f5d0894dfa05aa71db4bc8c7d0f30ce14e62e0b1
ethannicholsgithub <795318@seq.org>
2019-02-28 07:49:42 -0800

    f

--- ArtProject/sketch.js
+++ ArtProject/sketch.js
@@ -8,7 +8,7 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
-  loadBalls(4);
+  loadBalls(40);
   b1 = new Ball(createVector(width / 2, height / 2), createVector(0 , 0), 25, color(255, 0, 0));
 
 }
